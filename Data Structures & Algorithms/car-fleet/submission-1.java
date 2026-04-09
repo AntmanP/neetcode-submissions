9efd025aa65ea11fce6e1026ef29c2ffdf2166a4
Antarang Poogalia <antarangagent@gmail.com>
2026-04-09 17:10:03 -0500

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] pair = new int[position.length][2];
+        for (int i = 0; i < position.length; i++) {
+            pair[i][0] = position[i];
+            pair[i][1] = speed[i];
+        }
+
+        Arrays.sort(pair, (a, b) -> b[0] - a[0]);
+        int ans = 1;
+        // Stack<Double> stack = new Stack<>();
+        double prevtime = (double)(target - pair[0][0]) / pair[0][1];
+        for(int i=1;i<pair.length;i++){
+            double currtime = ((double)(target - pair[i][0]) / pair[i][1]) ;
+
+                if(currtime>prevtime){
+                    prevtime = currtime;
+                    ans++;
+                }
+            
+            }
+
+        return ans;
+
+    }
+}

f5ceb948987396214fff787f9558f7b7ee2896e1
Antarang Poogalia <antarangagent@gmail.com>
2026-05-13 14:51:46 -0500

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.java
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+         if (intervals.size() == 0) return 0;
+        intervals.sort((a, b) -> a.start - b.start);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.offer(intervals.get(0).end);
+
+        for(int i=1;i<intervals.size();i++){
+            if(!pq.isEmpty() && pq.peek()<=intervals.get(i).start){
+                pq.poll();
+            }
+            pq.offer(intervals.get(i).end);
+        }
+
+        return pq.size();
+    }
+}
